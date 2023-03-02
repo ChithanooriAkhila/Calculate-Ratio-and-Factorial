f2220dfd032200586075b1a299a5cf73caec088e
Chithanoori Akhila <70878045+ChithanooriAkhila@users.noreply.github.com>
2023-03-02 21:22:57 +0530

    Update index.js

--- utilities/ratioFactorial/index.js
+++ utilities/ratioFactorial/index.js
@@ -3,10 +3,10 @@ const factorial = require("../factorial/index");
 
 let obj = {};
 const ratioAndFactorial = (n1, n2, n3) => {
-  const ratio = ratio(n1, n2);
-  const factorial = factorial(n3);
-  obj["ratio"] = ratio;
-  obj["factorial"] = factorial;
+  const r = ratio(n1, n2);
+  const f = factorial(n3);
+  obj["ratio"] = r;
+  obj["factorial"] = f;
   return obj;
 };
 
