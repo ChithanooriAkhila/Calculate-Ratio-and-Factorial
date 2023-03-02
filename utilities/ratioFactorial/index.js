f03c6addebe26fff7390126abc5720961f220cb5
Chithanoori Akhila <70878045+ChithanooriAkhila@users.noreply.github.com>
2023-03-02 21:18:34 +0530

    Update index.js

--- utilities/ratioFactorial/index.js
+++ utilities/ratioFactorial/index.js
@@ -2,7 +2,7 @@ const ratioOfTwoNumbers = require("../ratio/index");
 const factorialOfNumber = require("../factorial/index");
 
 let obj = {};
-const ratioFactorial = (n1, n2, n3) => {
+const ratioAndFactorial = (n1, n2, n3) => {
   const ratio = ratioOfTwoNumbers(n1, n2);
   const factorial = factorialOfNumber(n3);
   obj["ratio"] = ratio;
@@ -10,6 +10,6 @@ const ratioFactorial = (n1, n2, n3) => {
   return obj;
 };
 
-console.log(ratioFactorial(2, 3, 4));
+console.log(ratioAndFactorial(2, 3, 4));
 
-module.exports = ratioFactorial;
+module.exports = ratioAndFactorial;
